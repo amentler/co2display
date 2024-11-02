dafa805e27a16ccbeba9023b74862ba433ddb317
Mentler, André <amentler@brockhaus-ag.de>
2024-11-02 21:01:29 +0100

    sensor programmiert mit deepsleep und allem. sensor misst alle 15 sekunden. angeblich dann <10ma verbrauch durch den sensor. ist aber trotzdem noch recht viel. alle 60 sekunden aufwachen aus dem deep sleep und evtl. anzeige auf dem display, wenn sich die werte ausreichen geändert haben.
    status LED vom firebeetle bleibt aus.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,151 @@
 #include <Arduino.h>
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include "sensor/SCD30.h"
+#include <Preferences.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.begin(9600);
+void sleepDeeply(int);
+void displayValues();
+bool readSensor();
+bool hasSensorValueChanged();
+bool isInRange(int, int, int);
+String getTrend(int, int);
+void updateStatistics();
+
+int lastHumidity = 0;
+int lastTemperature = 0;
+int lastCO2 = 0;
+
+int currentHumidity = 0;
+int currentTemperature = 0;
+int currentCO2 = 0;
+
+String humidityTrend = "-";
+String cO2Trend = "-";
+String temperatureTrend = "-";
+
+const String UP = "/\\";
+const String DOWN = "\\/";
+
+Preferences preferences;
+
+GxEPD2_BW<GxEPD2_154_GDEY0154D67, GxEPD2_154_GDEY0154D67::HEIGHT> display(
+  GxEPD2_154_GDEY0154D67(/*CS=D8*/ 0, /*DC=D3*/ 19, /*RST=D4*/ 21, /*BUSY=D2*/ 22)); // GDEY0154D67 200x200, SSD1681, (FPC-B001 20.05.21)
+
+
+void setup() {  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(115200);
+
+  // Initialize the preferences library
+  preferences.begin("co2sensor", false); // "my-app" is the namespace, false for read/write mode
+
+  scd30.initialize();
+  Serial.println("SCD30 initialized");  
+
+  display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse 
+  Serial.println("Display initialized");
+}
+
+void loadLastValues()
+{
+  lastCO2 = preferences.getInt("lastCO2", 0);
+  lastHumidity = preferences.getInt("lastHumidity", 0);
+  lastTemperature = preferences.getInt("lastTemperature", 0);
+
+  Serial.println("Values loaded: lastCO2: " + String(lastCO2) + " / lastHumidity: " + String(lastHumidity) + " / lastTemperature: " + String(lastTemperature) );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("hello world");
-  delay(1000);
+  runOnce();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void runOnce()
+{
+  loadLastValues();
+  if (readSensor() && hasSensorValueChanged())
+  {
+    updateStatistics();
+    displayValues();
+  }
+
+  sleepDeeply(120);
+}
+
+void sleepDeeply(int sleepSeconds)
+{
+  gpio_hold_en(GPIO_NUM_2);
+  gpio_deep_sleep_hold_en();
+  esp_deep_sleep(1000 * 1000 * sleepSeconds);
+}
+
+bool hasSensorValueChanged() {
+  return !(isInRange(currentCO2, lastCO2, 100) && isInRange(currentHumidity, lastHumidity, 2) && currentTemperature == lastTemperature);
+}
+
+void updateStatistics() {
+  Serial.println("updating Statistics!");
+  cO2Trend = getTrend(currentCO2, lastCO2);
+  Serial.println("currentCO2:" + String(currentCO2) + " lastCO2:" + String(lastCO2) + " Trend: " + String(cO2Trend));
+  lastCO2 = currentCO2;  
+
+  humidityTrend = getTrend(currentHumidity, lastHumidity);
+  Serial.println("currentHumidity:" + String(currentHumidity) + " lastHumidity:" + String(lastHumidity) + " Trend: " + String(humidityTrend));
+  lastHumidity = currentHumidity;
+
+  temperatureTrend = getTrend(currentTemperature, lastTemperature);
+  Serial.println("currentTemperature:" + String(currentTemperature) + " lastTemperature:" + String(lastTemperature) + " Trend: " + String(temperatureTrend));
+  lastTemperature = currentTemperature;
+
+  preferences.putInt("lastCO2", lastCO2);
+  preferences.putInt("lastHumidity", lastHumidity);
+  preferences.putInt("lastTemperature", lastTemperature);
+}
+
+String getTrend(int val1, int val2) {
+  if (val1==val2) return "-";
+  else if (val1>val2) return UP;
+  else return DOWN;
+}
+
+bool isInRange(int value, int lastvalue, int deviation) {
+  //Serial.println("isInRange value: " + String(value) + ", lastValue: " + String(lastvalue) + ", deviation: " + deviation); 
+  return ((value - deviation) < lastvalue) && (lastvalue < (value + deviation)); 
+}
+
+bool readSensor() {
+  if (scd30.isAvailable()) {
+    float result[3] = {0};
+    scd30.getCarbonDioxideConcentration(result);
+    currentCO2 = (int)round(result[0]);
+    currentTemperature = (int)round(result[1]);
+    currentHumidity = (int)round(result[2]);
+    Serial.println("currentCO2:" + String(currentCO2) + " currentTemperature:" + String(currentTemperature) + " currentHumidity: " + String(currentHumidity));
+    return true;
+  }
+  return false;
+}
+
+void displayValues()
+{
+    Serial.println("updating Display!");
+
+    String values = "\n" + String(currentCO2) + "ppm " + String(cO2Trend) + "\n \n" + 
+                    String(currentTemperature) + "C " + String(temperatureTrend) + "\n \n" + 
+                    String(currentHumidity) + "% " + String(humidityTrend) ;
+    Serial.println(values);
+        
+    display.setRotation(1);
+    display.setTextColor(GxEPD_BLACK);
+    display.setFullWindow();
+    display.firstPage();
+    do {
+        display.fillScreen(GxEPD_WHITE);
+        display.setCursor(0, 0);
+        display.setTextSize(3);
+        display.print(values);
+    } while (display.nextPage());
+    Serial.println("Display update is done");
 }
